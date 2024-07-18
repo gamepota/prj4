2641fbf04fdad92c4565742622bcabc934b88132
kdw9805 <kdw686968@gmail.com>
2024-07-18 11:08:28 +0900

    first commit

---
+++ src/main/java/com/prj4/Prj420240718Application.java
@@ -0,0 +1,13 @@
+package com.prj4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Prj420240718Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Prj420240718Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/prj4/Prj420240718ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prj4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prj420240718ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
