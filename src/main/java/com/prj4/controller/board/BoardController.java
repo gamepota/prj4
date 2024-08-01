7c5e77b65e417ec473654c7dd3241d4933e3e07d
kdw9805 <kdw686968@gmail.com>
2024-08-01 15:29:11 +0900

    BoardList List 띄우기 메소드

--- src/main/java/com/prj4/controller/board/BoardController.java
+++ src/main/java/com/prj4/controller/board/BoardController.java
@@ -4,10 +4,9 @@ import com.prj4.domain.board.Board;
 import com.prj4.service.board.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/board")
@@ -26,6 +25,11 @@ public class BoardController {
         } else {
             return ResponseEntity.badRequest().build();
         }
+    }
+
+    @GetMapping("list")
+    public List<Board> list() {
+        return service.list();
     }
 }
 
