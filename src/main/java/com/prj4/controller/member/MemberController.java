982aa0ad6013868412cd2a5acd43ffeb9ef231f5
kdw9805 <kdw686968@gmail.com>
2024-08-05 14:45:09 +0900

    회원 목록은 admin 만 볼 수 있게

--- src/main/java/com/prj4/controller/member/MemberController.java
+++ src/main/java/com/prj4/controller/member/MemberController.java
@@ -58,6 +58,7 @@ public class MemberController {
 
 
     @GetMapping("list")
+    @PreAuthorize("hasAuthority('SCOPE_admin')")
     public List<Member> list() {
         return service.list();
     }
